78cf2f3824e2b93756f4012af8224ccc80326aff
yt-hayashi <h.yuta999@gmail.com>
2018-08-06 15:08:09 +0900

    schemaの修正

---
+++ sql/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE user_table
+(
+    usr_name CHAR(15) PRIMARY KEY,
+    usr_password CHAR(20)  NOT NULL
+);
+
+CREATE TABLE post_table
+(
+    post_id CHAR(10) PRIMARY KEY,
+    usr_name CHAR(15),
+    img_name CHAR(100),
+    comment_id CHAR(100)
+);
+
+CREATE TABLE comment_table(
+    comment_id CHAR(10) PRIMARY KEY,
+    usr_name CHAR(15),
+    comment_value CHAR(255)
+);
