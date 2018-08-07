ed5c69395fa374b364d2fef4ae558219fd1e4780
yt-hayashi <h.yuta999@gmail.com>
2018-08-07 13:40:38 +0900

    templateを読み込めるように

--- app.go
+++ app.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"html/template"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	db, err := sql.Open("mysql", "root:mysql@tcp(127.0.0.1:33306)/test")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
+	if err := db.Ping(); err != nil {
+		fmt.Println("DB Error! --> ", err.Error())
+		os.Exit(1)
+	}
+
+	http.HandleFunc("/", top)
 	http.ListenAndServe(":8080", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, Golang!")
+func top(w http.ResponseWriter, r *http.Request) {
+	tmp := template.Must(template.ParseFiles("template/top.html.tpl"))
+
+	textBody := "This is Test."
+
+	if err := tmp.ExecuteTemplate(w, "top.html.tpl", textBody); err != nil {
+		fmt.Println(err.Error())
+	}
 }
