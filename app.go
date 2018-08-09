b0c739cc416944617ac1e5355b3088724ee7f71b
hayashi-yuta <hayashi-yuta@KYCNT1704-27.local>
2018-08-09 11:38:04 +0900

    sessionにidが保存されるように

--- app.go
+++ app.go
@@ -69,7 +69,7 @@ func top(w http.ResponseWriter, r *http.Request) {
 
 	//session 読み出し
 	session, err := store.Get(r, "user-session")
-	fmt.Println(session.Values["userName"])
+	fmt.Println(session.Values["userID"])
 
 	if err := tmp.ExecuteTemplate(w, "top.html.tpl", posts); err != nil {
 		fmt.Println(err.Error())
@@ -151,7 +151,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 		// login時の処理
 
 		//DBから読み出し
-		rows, err := db.Query("SELECT name, password FROM users WHERE name=?", userName)
+		rows, err := db.Query("SELECT id, name, password FROM users WHERE name=?", userName)
 		if err != nil {
 			fmt.Println(err.Error())
 			w.WriteHeader(http.StatusInternalServerError)
@@ -160,8 +160,9 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 		_userName := ""
 		_password := ""
+		var id int
 		for rows.Next() {
-			if err := rows.Scan(&_userName, &_password); err != nil {
+			if err := rows.Scan(&id, &_userName, &_password); err != nil {
 				fmt.Println(err.Error())
 				w.WriteHeader(http.StatusInternalServerError)
 				return
@@ -186,6 +187,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		session.Values["userName"] = userName
+		session.Values["userID"] = id
 		// Save it before we write to the response/return from the handler.
 		session.Save(r, w)
 		http.Redirect(w, r, "/", http.StatusSeeOther)
